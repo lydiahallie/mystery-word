39ffd98af85859268bffb68ac9ff74f644c5b8b1
lydiahallie <lydiajuliettehallie@gmail.com>
2017-07-30 10:41:40 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const mustacheExpress = require('mustache-express')
+const app = express()
+const fs = require('fs')
+const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+// Use Mustache as our templating engine.
+app.engine('mustache', mustacheExpress())
+app.set('views', './views')
+app.set('view engine', 'mustache')
+
+app.use(express.static('./public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req,res) => {
+  res.render('home')
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
