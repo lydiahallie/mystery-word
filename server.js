55bbd2425a85902beed46a6a18ed785c71409e7f
lydiahallie <lydiajuliettehallie@gmail.com>
2017-07-31 17:45:30 -0400

    Added layout and changed server.js

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const mustacheExpress = require('mustache-express')
-const app = express()
 const fs = require('fs')
 const words = fs.readFileSync("/usr/share/dict/words", "utf-8").toLowerCase().split("\n");
+const expressValidator = require('express-validator')
 
-let underscores = [];
-let count = 8;
+const app = express();
 
 app.engine('mustache', mustacheExpress())
 app.set('views', './views')
@@ -16,10 +15,29 @@ app.use(express.static('./public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+let attempts;
+let counter;
+let guessWord = words[Math.floor(Math.random()*words.length)];
+let underscores = guessWord.replace(/[a-z]/gi, '_');
+
 app.get('/', (req,res) => {
+  attempts = [];
+  counter = 0;
   res.render('home')
 })
+
+app.post('/attempt', (req, res) => {
+  req
+  .checkBody("attemptedLetter", "It has to be a valid letter")
+  .isAlpha()
+  .notEmpty()
+})
+
 
 app.listen(3000, () => {
   console.log('Listening on port 3000')
 })
+
+// Create an input field
+// Create an array, push the req.body of the input field to that array (attempts)
+// Give the Guess button a function
