c1cb6bd2e762a409360053bd28537ed99f9e3a57
lydiahallie <lydiajuliettehallie@gmail.com>
2017-07-31 19:09:24 -0400

    The underscores show up now

--- server.js
+++ server.js
@@ -21,18 +21,21 @@ let attempts;
 let counter;
 let underscores = [];
 
-//it has to pick a random word from the file
-const guessWord = words[Math.floor(Math.random()*words.length)];
-
-//get the underscores instead of the actual letters for now
-const wordToGuess = guessWord.replace(/[a-z]/gi, '_');
+//it has to pick a random word from the file, then replace the
+//letters with underscores, then return that
+const getWord = () => {
+  const word = words[Math.floor(Math.random()*words.length)];
+  const hiddenWord = word.replace(/[a-z]/gi, '_');
+  return hiddenWord
+}
 
 //whenever you load the page, it'll set the counter to 0 and pushes
 //the word that it found to the underscores empty array
 app.get('/', (req,res) => {
   counter = 0;
-  underscores.push(wordToGuess);
-  res.render('home');
+  let guessWord = getWord()
+  fullWord = guessWord
+  res.render('home', {guessWord, fullWord});
 })
 
 // this isn't working yet, but when you click on the button it should
