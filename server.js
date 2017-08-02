39275df240dd6eabb9408b46d2890d06890a5a72
lydiahallie <lydiajuliettehallie@gmail.com>
2017-08-02 12:50:30 -0400

    Working, need lose page

--- server.js
+++ server.js
@@ -17,36 +17,56 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 let counterAttempts;
 let attemptedLetters = [];
+let message;
+let result;
+let loseMessage;
 //To show how many attempts they have left, the counter of that,
 // and the underscores that will appear
 const word = words[Math.floor(Math.random()*words.length)];
 
 //Underscores
 const hideWord = (word) => word.split('').map(character => '_').join('');
+let displayedWord = hideWord(word).split('');
 //whenever you load the page, it'll set the counter to 0 and pushes
 //the word that it found to the underscores empty array
 app.get('/', (req,res) => {
   const displayedWord = hideWord(word);
-  counterAttempts = 0;
-  res.render('home', { word, displayedWord });
+  counterAttempts = 8;
+  res.render('home', { word, displayedWord, counterAttempts});
 })
 
 // this isn't working yet, but when you click on the button it should
 // check whether the input is valid
 app.post('/attempt', (req, res) => {
   const guessedLetter = req.body.letter.toLowerCase();
-  let displayedWord = hideWord(word).split('');
   for (let i = 0; i < word.length; i++) {
     const displayedLetter = word[i];
     if (displayedLetter === guessedLetter) {
       displayedWord[i] = displayedLetter;
     }
-    if (!word.includes(guessedLetter)) {
-      counterAttempts = (counterAttempts+1)
+  }
+
+  if (attemptedLetters.includes(guessedLetter)) {
+    message = "You have already guessed this letter!"
+  }
+
+  if (!word.includes(guessedLetter) && (!attemptedLetters.includes(guessedLetter))) {
+    if (counterAttempts === 1){
+      loseMessage = "You lose!"
+      res.redirect('lose', {loseMessage})
+    }else{
+      counterAttempts--
     }
   }
-  attemptedLetters.push(guessedLetter);
-  res.render('home', { displayedWord: displayedWord.join(''), counterAttempts, attemptedLetters});
+
+  if (!attemptedLetters.includes(guessedLetter)){
+    attemptedLetters.push(guessedLetter);
+  }
+
+  //It has to show the word when the player doesn't get it right
+
+  //If the player loses or wins, it has to show a "Play again?" button
+  res.render('home', { displayedWord: displayedWord.join(''), counterAttempts, attemptedLetters, message, loseMessage});
 });
 
 
